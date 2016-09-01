12287dc7d181583f034fd576c5ef52e6780ddafd
starseac <851388951@qq.com>
2016-09-01 14:37:55 +0800

    高排仪测试代码

---
+++ Starsea.SCANNER/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SCANNER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axDoccameraOcx1 = new AxDOCCAMERAOCXLib.AxDoccameraOcx();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axDoccameraOcx1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axDoccameraOcx1
+            // 
+            this.axDoccameraOcx1.Enabled = true;
+            this.axDoccameraOcx1.Location = new System.Drawing.Point(12, 12);
+            this.axDoccameraOcx1.Name = "axDoccameraOcx1";
+            this.axDoccameraOcx1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axDoccameraOcx1.OcxState")));
+            this.axDoccameraOcx1.Size = new System.Drawing.Size(539, 550);
+            this.axDoccameraOcx1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(789, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(789, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "拍摄";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(789, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 31);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "保存";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(789, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 31);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "预览";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(789, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 31);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "上传";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 735);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axDoccameraOcx1);
+            this.Name = "Form1";
+            this.Text = "扫描上传";
+            ((System.ComponentModel.ISupportInitialize)(this.axDoccameraOcx1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxDOCCAMERAOCXLib.AxDoccameraOcx axDoccameraOcx1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Starsea.SCANNER/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SCANNER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.button1.Text != "停止")
+            {
+                axDoccameraOcx1.bStopPlay();
+                //axDoccameraOcx1.vSetSkewFlag(true);
+                //axDoccameraOcx1.vSetDelHBFlag(true);
+                axDoccameraOcx1.bStartPlay();
+                this.button1.Text = "停止";
+            }
+            else
+            {
+                axDoccameraOcx1.bStopPlay();
+                this.button1.Text = "启动";
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.button2.Text != "继续拍摄")
+            {
+                axDoccameraOcx1.bSavePDFStart(@"c:\", "test");
+
+                this.button2.Text = "继续拍摄";
+            }
+            else
+            {
+                axDoccameraOcx1.bSavePDFColorPage();
+
+            }
+        }
+
+       
+        private void button4_Click(object sender, EventArgs e)
+        {
+             axDoccameraOcx1.bSavePDFColorPage();
+            axDoccameraOcx1.bSavePDFEnd();
+            if (this.button2.Text == "继续拍摄")
+            {
+                this.button2.Text = "拍摄";
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
